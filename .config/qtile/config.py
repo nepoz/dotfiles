0690118e951f1ea20601088ef2749cb33bd3eb1e
nepoz <bishrutbhattarai@gmail.com>
2022-08-27 12:33:37 -0500

    add wallpaper support

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -92,7 +92,7 @@ groups = [
     Group("www", persist=False),
     Group("dev", persist=False),
     Group("VM", persist = False),
-    Group("discord", spawn=["discord"],  persist=False, init=True),
+    Group("discord", spawn=["discord", "spotify"],  persist=False, init=True),
     Group("shh", persist=False),
 ]
 
@@ -151,6 +151,8 @@ screens = [
             # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
             # border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
         ),
+        wallpaper="~/.wallpaper/bluesky.png",
+        wallpaper_mode="fill",
     ),
 ]
 
