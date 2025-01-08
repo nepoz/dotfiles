3c81e2d884206c869e2b8e205cbef39514998454
nepoz <bishrut@avinto.no>
2025-01-08 19:15:48 +0545

    feat: fix color of single col

--- nvim/init.lua
+++ nvim/init.lua
@@ -10,7 +10,7 @@ vim.opt.fillchars = "eob: "
 
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.smartcase = true
+vim.opt.ignorecase = true
 vim.opt.colorcolumn = "80"
 
 vim.opt.expandtab = true
@@ -23,3 +23,4 @@ vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
 vim.api.nvim_set_hl(0, "CursorLine", { bg = "#2e3440", underline = false })
 vim.api.nvim_set_hl(0, "ColorColumn", { bg = "#2e3440" })
+vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
